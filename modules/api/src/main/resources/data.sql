4cdebd2143f6726af17dc88012bbb9e75b3713a5
Kristianto Yanuar <kristianto.yanuar@icloud.com>
2017-02-02 18:02:54 +0800

    Remove example codes

--- modules/api/src/main/resources/data.sql
+++ modules/api/src/main/resources/data.sql
@@ -1,9 +1,6 @@
 insert into user_account (id, email, username, password) values
   (1, 'admin@somewhere.com', 'admin', '$2a$06$Ci6H7eaVno04rjyX8qtWaOUT38T32TIQiNz3R4QuBVL9cJxLfODRG'); -- password is 'ultrasecret'
 
-insert into student (first_name, last_name, code) values
-  ('John', 'Doe', 'S_01');
-
 insert into api_client(client_id, client_name, is_client_secret_required, is_scoped) values
   ('ui_client', 'Default UI Client', false, false);
 
